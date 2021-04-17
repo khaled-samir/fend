c23126616c8fdce3f68782fc14cddb52f001d020
khaled <khaled@zealous.co>
2021-04-17 05:13:49 +0200

    Fix API issues

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,42 +1,43 @@
+import { validURL } from "./validURL";
+
 function handleSubmit(event) {
     event.preventDefault()
-    let formText = document.getElementById('name').value
+    let formText = document.getElementById('name').value;
+    if (!validURL(formText)) {
+        alert("please enter valid URL");
+        return;
+    } else {
+        const formdata = new FormData();
+        formdata.append("key", "9d87555e8faa41de323561e444471bfb");
+        formdata.append("url", formText);
+        formdata.append("lang", "en");  // 2-letter code, like en es fr ...
 
-    const formdata = new FormData();
-    formdata.append("key", "9d87555e8faa41de323561e444471bfb");
-    formdata.append("txt", "egypt and franc going to world cup");
-    formdata.append("lang", "en");  // 2-letter code, like en es fr ...
+        const requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        };
 
-    const requestOptions = {
-        method: 'POST',
-        body: formdata,
-        redirect: 'follow'
-    };
+        const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+            .then(response => ({
+                status: response.status,
+                body: response.json()
+            }))
+            .then(({ status, body }) => {
+                console.log(status, body)
+                return body;
+            }).then((response) => {
+                document.getElementById('results').innerHTML =
+                    (response.subjectivity ? 'Score Tag: ' + response.score_tag + '<br>' : '') +
+                    (response.subjectivity ? 'Agreement: ' + response.agreement + '<br>' : '') +
+                    (response.subjectivity ? 'Subjectivity: ' + response.subjectivity + '<br>' : '') +
+                    (response.subjectivity ? 'Confidence: ' + response.confidence + '<br>' : '') +
+                    (response.subjectivity ? 'Irony: ' + response.irony + '<br>' : '');
+                // document.getElementById('results').innerHTML += JSON.stringify(response);
+                console.log(response)
+            })
+            .catch(error => console.log('error', error));
+    }
 
-    const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
-        .then(response => ({
-            status: response.status,
-            body: response.json()
-        }))
-        .then(({ status, body }) => {
-            console.log(status, body)
-            return body;
-        }).then((response) => {
-            document.getElementById('results').innerHTML += "subjectivity: " + response.subjectivity;
-            document.getElementById('results').innerHTML += JSON.stringify(response);
-            console.log(response)
-        })
-        .catch(error => console.log('error', error));
-
-    // // check what text was put into the form field
-    // let formText = document.getElementById('name').value
-    // Client.checkForName(formText)
-
-    // console.log("::: Form Submitted :::")
-    // fetch('https://api.meaningcloud.com/sentiment-2.1')
-    // .then(res => res.json())
-    // .then(function(res) {
-    //     document.getElementById('results').innerHTML = res.status.msg
-    // })
 }
 export { handleSubmit }
